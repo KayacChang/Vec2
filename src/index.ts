6df6e60e91f13efd961d8b65fed44fb4564fc0f8
kayac <egg734631@gmail.com>
2020-08-15 01:31:34 +0800

    Add lerp

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,12 @@ export interface IVec3 extends IVec2 {
   z: number;
 }
 
+type AVec2 = [number, number];
+
+function isAVec2(o: any): o is AVec2 {
+  return isNumberArray(o) && o.length === 2;
+}
+
 function vec2_from(x: number, y: number) {
   return Object.defineProperties([], {
     [0]: {
@@ -86,7 +92,7 @@ export function Vec2(obj: { x: number; y: number }): IVec2;
 export function Vec2(arr: [number, number]): IVec2;
 export function Vec2(...args: any): IVec2 {
   if (args.length === 1) {
-    if (isNumberArray(args[0]) && args[0].length === 2) {
+    if (isAVec2(args[0])) {
       const [x, y] = args[0];
       return vec2_from(x, y);
     }
@@ -105,7 +111,7 @@ export function Vec2(...args: any): IVec2 {
 }
 
 export function isVec2(o: any): o is IVec2 {
-  return isNumberArray(o) && o.length === 2 && "x" in o && "y" in o;
+  return isAVec2(o) && "x" in o && "y" in o;
 }
 
 export function isVec3(o: any): o is IVec3 {
@@ -120,16 +126,25 @@ export function sub([a1, a2]: IVec2, [b1, b2]: IVec2): IVec2 {
   return Vec2([a1 - b1, a2 - b2]);
 }
 
-export function mul(a: IVec2, b: IVec2): IVec2;
+export function mul(a: AVec2, b: number): IVec2;
+export function mul(a: AVec2, b: AVec2): IVec2;
+export function mul(a: AVec2, b: IVec2): IVec2;
 export function mul(a: IVec2, b: number): IVec2;
-export function mul(a: IVec2, b: any): IVec2 {
-  if (isVec2(b)) {
+export function mul(a: IVec2, b: AVec2): IVec2;
+export function mul(a: IVec2, b: IVec2): IVec2;
+export function mul(a: any, b: any): IVec2 {
+  if (
+    (isVec2(a) && isVec2(b)) ||
+    (isVec2(a) && isAVec2(b)) ||
+    (isAVec2(a) && isAVec2(b)) ||
+    (isAVec2(a) && isVec2(b))
+  ) {
     const [a1, a2] = a;
     const [b1, b2] = b;
     return Vec2([a1 * b1, a2 * b2]);
   }
 
-  if (isNumber(b)) {
+  if ((isVec2(a) && isNumber(b)) || (isAVec2(a) && isNumber(b))) {
     const [a1, a2] = a;
     return Vec2([a1 * b, a2 * b]);
   }
@@ -145,8 +160,24 @@ export function mag([x, y]: IVec2): number {
   return Math.sqrt(x * x + y * y);
 }
 
-export function dot([a1, a2]: IVec2, [b1, b2]: IVec2): number {
-  return a1 * b1 + a2 * b2;
+export function dot(a: AVec2, b: IVec2): number;
+export function dot(a: AVec2, b: AVec2): number;
+export function dot(a: IVec2, b: AVec2): number;
+export function dot(a: IVec2, b: IVec2): number;
+export function dot(a: any, b: any): number {
+  if (
+    (isVec2(a) && isVec2(b)) ||
+    (isVec2(a) && isAVec2(b)) ||
+    (isAVec2(a) && isAVec2(b)) ||
+    (isAVec2(a) && isVec2(b))
+  ) {
+    const [a1, a2] = a;
+    const [b1, b2] = b;
+
+    return a1 * b1 + a2 * b2;
+  }
+
+  throw new Error(`Invalid argument type`);
 }
 
 export function cross([a1, a2, a3]: IVec3, [b1, b2, b3]: IVec3): IVec3 {
@@ -175,4 +206,37 @@ export function det([top, down]: [IVec2, IVec2]): number {
   const [a, b] = top;
   const [c, d] = down;
   return a * d - b * c;
+}
+
+function clamp(a: number, min = 0, max = 1): number {
+  return Math.min(max, Math.min(min, a));
+}
+
+function _lerp(x: number, y: number, t: number): number {
+  t = clamp(t);
+  return x * (1 - t) + y * t;
+}
+
+export function lerp(a: AVec2, b: AVec2, t: number): IVec2;
+export function lerp(a: IVec2, b: AVec2, t: number): IVec2;
+export function lerp(a: AVec2, b: IVec2, t: number): IVec2;
+export function lerp(a: IVec2, b: IVec2, t: number): IVec2;
+export function lerp(a: any, b: any, t: number): IVec2 {
+  if (
+    (isAVec2(a) && isAVec2(b)) ||
+    (isVec2(a) && isVec2(b)) ||
+    (isAVec2(a) && isVec2(b)) ||
+    (isVec2(a) && isAVec2(b))
+  ) {
+    const [a1, a2] = a;
+    const [b1, b2] = b;
+
+    // prettier-ignore
+    return Vec2([
+      _lerp(a1, b1, t),
+      _lerp(a2, b2, t),
+    ]);
+  }
+
+  throw new Error(`Invalid argument type`);
 }
