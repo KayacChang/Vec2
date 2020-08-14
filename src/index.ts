23b22b66bc0e46b2563ac8b262680d254de424a6
kayac <egg734631@gmail.com>
2020-08-14 22:37:05 +0800

    Add

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 export type Vec2 = [number, number];
+export type Vec3 = [number, number, number];
+
+function isNumber(o: any): o is number {
+  return !Number.isNaN(o);
+}
 
 export function isVec2(o: any): o is Vec2 {
-  return (
-    Array.isArray(o) && o.length === 2 && o.every((el) => !Number.isNaN(el))
-  );
+  return Array.isArray(o) && o.length === 2 && o.every(isNumber);
 }
 
 export function add([a1, a2]: Vec2, [b1, b2]: Vec2): Vec2 {
@@ -20,13 +23,11 @@ export function mul(a: Vec2, b: any): Vec2 {
   if (isVec2(b)) {
     const [a1, a2] = a;
     const [b1, b2] = b;
-
     return [a1 * b1, a2 * b2];
   }
 
-  if (!Number.isNaN(b)) {
+  if (isNumber(b)) {
     const [a1, a2] = a;
-
     return [a1 * b, a2 * b];
   }
 
@@ -45,13 +46,30 @@ export function dot([a1, a2]: Vec2, [b1, b2]: Vec2): number {
   return a1 * b1 + a2 * b2;
 }
 
+export function cross([a1, a2, a3]: Vec3, [b1, b2, b3]: Vec3): Vec3 {
+  // prettier-ignore
+  return [
+    a2 * b3 - a3 * b2,
+    a3 * b1 - a1 * b3,
+    a1 * b2 - a2 * b1,
+  ];
+}
+
+export function rotate(v: Vec2, radian: number): Vec2 {
+  const { cos, sin } = Math;
+  return [
+    dot(v, [cos(radian), -1 * sin(radian)]),
+    dot(v, [sin(radian), cos(radian)]),
+  ];
+}
+
 export function normal(v: Vec2): Vec2 {
-  return div(v, mag(v));
+  const { max } = Math;
+  return div(v, max(1, mag(v)));
 }
 
 export function det([top, down]: [Vec2, Vec2]): number {
   const [a, b] = top;
   const [c, d] = down;
-
   return a * d - b * c;
 }
