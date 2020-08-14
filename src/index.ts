760eaa0d0ece67298e0bd248e946d2f6df66d7a2
kayac <egg734631@gmail.com>
2020-08-14 21:51:58 +0800

    Add

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,55 @@
 export type Vec2 = [number, number];
 
-export function mag([x, y]: Vec2) {
+export function isVec2(o: any): o is Vec2 {
+  return (
+    Array.isArray(o) && o.length === 2 && o.every((el) => !Number.isNaN(el))
+  );
+}
+
+export function add([a1, a2]: Vec2, [b1, b2]: Vec2): Vec2 {
+  return [a1 + b1, a2 + b2];
+}
+
+export function sub([a1, a2]: Vec2, [b1, b2]: Vec2): Vec2 {
+  return [a1 - b1, a2 - b2];
+}
+
+export function mul(a: Vec2, b: Vec2): Vec2;
+export function mul(a: Vec2, b: number): Vec2;
+export function mul(a: Vec2, b: any): Vec2 {
+  if (isVec2(b)) {
+    const [a1, a2] = a;
+    const [b1, b2] = b;
+
+    return [a1 * b1, a2 * b2];
+  }
+
+  if (!Number.isNaN(b)) {
+    const [a1, a2] = a;
+
+    return [a1 * b, a2 * b];
+  }
+
+  throw new Error(`Invalid argument type b ${b} is ${typeof b}`);
+}
+
+export function div([x, y]: Vec2, s: number): Vec2 {
+  return [x / s, y / s];
+}
+
+export function mag([x, y]: Vec2): number {
   return Math.sqrt(x * x + y * y);
 }
 
-export function dot([x1, y1]: Vec2, [x2, y2]: Vec2) {
-  return x1 * x2 + y1 * y2;
+export function dot([a1, a2]: Vec2, [b1, b2]: Vec2): number {
+  return a1 * b1 + a2 * b2;
 }
 
-export function mul([x, y]: Vec2, scaler: number): Vec2 {
-  return [x * scaler, y * scaler];
+export function normal(v: Vec2): Vec2 {
+  return div(v, mag(v));
 }
 
-export function divide([x, y]: Vec2, scaler: number): Vec2 {
-  return [x / scaler, y / scaler];
-}
-
-export function normalize(v: Vec2) {
-  return divide(v, mag(v));
-}
-
-export function add([x1, y1]: Vec2, [x2, y2]: Vec2): Vec2 {
-  return [x1 + x2, y1 + y2];
-}
-
-export function sub([x1, y1]: Vec2, [x2, y2]: Vec2): Vec2 {
-  return [x1 - x2, y1 - y2];
-}
-
-export function det([top, down]: [Vec2, Vec2]) {
+export function det([top, down]: [Vec2, Vec2]): number {
   const [a, b] = top;
   const [c, d] = down;
 
